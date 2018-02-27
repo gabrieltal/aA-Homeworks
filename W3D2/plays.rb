0376aee073cc4b3e332400fb41fe326707ceee31
Live session user <gabrielvt6@gmail.com>
2018-02-26 20:00:06 -0800

    Made plays.db, work on some class methods

--- W3D2/plays.rb
+++ W3D2/plays.rb
@@ -11,6 +11,71 @@ class PlayDBConnection < SQLite3::Database
   end
 end
 
+class Playwright
+  attr_accessor :name, :birthyear
+  attr_reader :id
+
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+    data.map { |datum| Playwright.new(datum)}
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birthyear = options['birthyear']
+  end
+
+  def self.find_by_name(name)
+    playwright = PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT
+        *
+      FROM
+        playwrights
+      WHERE
+        name = ?
+    SQL
+    return nil if person.length <= 0
+    Playwright.new(person.first)
+  end
+
+  def create
+    raise "#{self} already in database" if @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birthyear)
+      INSERT INTO
+        playwrights (name, birthyear)
+      VALUES
+        (?, ?)
+    SQL
+    @id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless @id
+    PlayDBConnection.instance(<<-SQL, @name, @birthyear, @id)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birthyear = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def get_plays
+    raise "#{self} not in database" unless @id
+    plays = PlayDBConnection.instance.execute(<<-SQL, @id)
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        playwright_id = ?
+    SQL
+    plays.map { |play| Play.new(play) }
+  end
+end
+
 class Play
   attr_accessor :title, :year, :playwright_id
 
@@ -47,5 +112,33 @@ class Play
       WHERE
         id = ?
     SQL
+  end
+
+  def self.find_by_title(title)
+    play = PlayDBConnection.instance.execute(<<-SQL, title)
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        title = ?
+    SQL
+    return nil unless play.length > 0
+    Play.new(play.first)
+  end
+
+  def self.find_by_playwright(name)
+    playwright = Playwright.find_by_name(name)
+
+    plays = PlayDBConnection.instance.execute(<<-SQL, playwright.id)
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        playwright_id = ?
+    SQL
+
+    plays.map { |play| Play.new(play) }
   end
 end
